15ee88bef29b2c696e68363bb2e973c929b55b4b
Bram van Osta <bramvanosta@gmail.com>
2019-10-19 17:56:15 +0200

    fix: Forgot to remove imports to removed files

--- src/app/Types/Models/index.ts
+++ src/app/Types/Models/index.ts
@@ -1,7 +1,6 @@
 export { default as BaseModel } from './BaseModel';
 export { default as IEstablishment } from './Establishment';
 export { default as IEstablishmentToken } from './EstablishmentToken';
-export { default as IEstablishmentUserPermission } from './EstablishmentUserPermission';
 export { default as IPermission } from './Permission';
 export { default as IRental } from './Rental';
 export { default as IRentalsCharacteristics } from './RentalsCharacteristics';
