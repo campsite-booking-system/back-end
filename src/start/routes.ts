f7c97d0ee35c76a30f4b68a3a6436e68a962985a
Bram van Osta <bramvanosta@gmail.com>
2019-11-11 19:28:37 +0100

    feature: Add wildcard route to 404 exception

--- src/start/routes.ts
+++ src/start/routes.ts
@@ -1,6 +1,12 @@
+import { NotFoundException } from '../app/Exceptions';
+
 const Route = use('Route');
 
 Route.get('/', () => 'Welcome to the Vulpee API');
 
 import './routes/V1/public';
 import './routes/V1/private';
+
+Route.any('*', () => {
+  throw new NotFoundException();
+});
